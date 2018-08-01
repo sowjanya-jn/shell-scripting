1cadd16bacf73cad45d540d0ffe3dcc02edc26de
sowjanya <soujanya.jn@gmail.com>
2018-08-01 17:21:14 +0530

    git commands

--- basic_scripting.sh
+++ basic_scripting.sh
@@ -1,2 +1,22 @@
 echo " today is`date` "
  cal
+
+ git clone https://github.com/sowjanya-jn/shell-scripting.git
+
+ cat file_name
+
+ ls file_name/
+
+ sudo apt install vim
+
+ vim file_name.sh
+
+ chmod u+x,g+wx,o+wx file_name.sh
+
+ sh file_name.sh
+
+ git add file_name
+
+ git commit -m "message"
+
+ git push origin master
